b3ed936c10c8cb762cd92fa5034aa4fbd3a7d0f8
Bruno Dall <brunodall_@hotmail.com>
2025-10-03 17:03:09 -0300

    Refactor lead synchronization and leads display pages; remove old implementations and introduce new structure for car management features.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'core/di/injection_container.dart' as di;
-import 'features/cars/presentation/page/car_page.dart';
+import 'features/cars/presentation/pages/car_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
